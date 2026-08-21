e66807363a3b1966c7815e2d1d09793bb39a67e4
deksut2012 <deksut2012@gmail.com>
2026-08-21 19:00:18 +0700

    chore: remove legacy startup batch scripts

--- OPEN_SYSTEM_MANAGER.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if exist ".artifacts\system-manager\ProMaxx2.ServiceManager.exe" (
-    start "" ".artifacts\system-manager\ProMaxx2.ServiceManager.exe"
-    exit /b 0
-)
-dotnet run --project "tools\ProMaxx2.ServiceManager\ProMaxx2.ServiceManager.csproj"
-if errorlevel 1 pause
--- START_DEV_SERVER.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Start ProMaxx2 QA Web Dev Server
-cd /d "%~dp0src\ProMaxx2.QA.Web"
-echo Starting Vite Dev Server on http://192.168.200.219:5173/
-echo.
-npm run dev
-pause
